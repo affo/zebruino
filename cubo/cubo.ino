a9a92f7b928941da7137fb3f2b95be1c4317ff9e
Lorenzo Affetti <affox90@gmail.com>
2014-06-28 15:50:16 +0200

    println added

--- cubo/cubo.ino
+++ cubo/cubo.ino
@@ -1,4 +1,4 @@
-const int LOOP_DELAY = 500;
+const int LOOP_DELAY = 3000;
 // PIN
 // pin dei led
 int led1_pin = 2, led2_pin = 3, led_voice_pin = 4;
@@ -29,18 +29,26 @@ void loop(){
 		if(rand_num < 0.5){
 			digitalWrite(led1_pin, HIGH);
 			led1_status = HIGH;
+
+			Serial.println("RED_LED_1: ON");
 		}else{
 			digitalWrite(led1_pin, LOW);
 			led1_status = LOW;
+
+			Serial.println("RED_LED_1: OFF");
 		}
 
 		rand_num = random(0, 1);
 		if(rand_num < 0.5){
 			digitalWrite(led2_pin, HIGH);
 			led2_status = HIGH;
+
+			Serial.println("RED_LED_2: ON");
 		}else{
 			digitalWrite(led2_pin, LOW);
 			led2_status = LOW;
+
+			Serial.println("RED_LED_2: OFF");
 		}
 
 		// se tutti e due i led sono accesi
@@ -48,16 +56,26 @@ void loop(){
 		if(led1_status == HIGH && led2_status == HIGH){
 			// accendi il led verde
 			digitalWrite(led_voice_pin, HIGH);
-			read_mic = true;			
+			read_mic = true;
+
+			Serial.println("GREEN_LED: ON (led1 on && led2 on)");			
 		}else{
 			digitalWrite(led_voice_pin, LOW);
 			read_mic = false;
+
+			Serial.println("GREEN_LED: OFF (led1 off || led2 off)");
 		}
 
 	}else{
-		rand_num = int(random(0, 255));
-		analogWrite(led_voice_pin, rand_num);
-		read_mic = false;
+		//rand_num = int(random(0, 255));
+		//analogWrite(led_voice_pin, rand_num);
+		rand_num = random(0, 1);
+		if(rand_num < 0.5){
+			digitalWrite(led_voice_pin, HIGH);
+			read_mic = false;
+
+			Serial.println("GREEN_LED: OFF (randomly)");
+		}
 	}
 
 	delay(LOOP_DELAY);
