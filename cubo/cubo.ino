4a49f3d3c5079523bf88e4005e3f68670e09fe9a
Lorenzo Affetti <affox90@gmail.com>
2014-06-28 11:35:40 +0200

    setup phase started

---
+++ cubo/cubo.ino
@@ -0,0 +1,20 @@
+// pin dei sensori di prossimità
+int proximity1, proximity2;
+// pin dei led
+int led1, led2, led_voice;
+// pin del microfono
+int mic;
+
+
+void setup(){
+  pinMode(proximity1, INPUT);
+  pinMode(proximity2, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led_voice, OUTPUT);
+  pinMode(mic, INPUT);
+}
+
+void loop(){
+  
+}
