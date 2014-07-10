19bee7f43e7bb2cf167d3d861d00d142173430b1
Lorenzo Affetti <affox90@gmail.com>
2014-07-10 14:23:47 +0200

    trying to avoid flickering

--- cubo/cubo.ino
+++ cubo/cubo.ino
@@ -1,4 +1,5 @@
 #define LOOP_DELAY 10
+#define MEASUREMENT_SPAN 10
 
 // PIN
 // pin dei sensori di prossimità
@@ -14,8 +15,47 @@ int pin_mic = A0;
 // rumore di fondo
 int noise;
 
-int last_prox_1;
-int last_prox_2; 
+// togliere il flickering dai sensori di prossimità
+int last_prox_1[MEASUREMENT_SPAN], index_1 = 0;
+int last_prox_2[MEASUREMENT_SPAN], index_2 = 0;
+
+void add_prox_1(int val){
+	last_prox_1[index_1] = val;
+	index_1 ++;
+	if(index_1 == MEASUREMENT_SPAN){
+		index_1 = 0;
+	}
+} 
+
+void add_prox_2(int val){
+	last_prox_2[index_2] = val;
+	index_2 ++;
+	if(index_2 == MEASUREMENT_SPAN){
+		index_2 = 0;
+	}
+}
+
+int get_prox_1(void){
+	int result = HIGH;
+	for(int i = 0; i < MEASUREMENT_SPAN; i++){
+		result = result && !last_prox_1[i];
+		if(!result){
+			break;
+		}
+	}
+	return result;
+}
+
+int get_prox_2(void){
+	int result = HIGH;
+	for(int i = 0; i < MEASUREMENT_SPAN; i++){
+		result = result && !last_prox_2[i];
+		if(!result){
+			break;
+		}
+	}
+	return result;
+}
 
 // funzione creata per non permettere che il led vada in "flickering"
 // ritorna 4 valori predefiniti al posto dei 256 possibili.
@@ -59,40 +99,37 @@ void setup(){
 	pinMode(pin_mic, INPUT);
 
 	noise = analogRead(pin_mic);
-	last_prox_1 = HIGH;
-	last_prox_2 = HIGH;
+
+	for(int i = 0; i < MEASUREMENT_SPAN; i++){
+		last_prox_1[i] = HIGH;
+		last_prox_2[i] = HIGH;
+	}
 }
 
 void loop(){
 	// prendo i valori dai sensori di presenza
 	int status_prox_1 = digitalRead(pin_prox_1);
 	int status_prox_2 = digitalRead(pin_prox_2);
+	add_prox_1(status_prox_1);
+	add_prox_2(status_prox_2);
+	status_prox_1 = get_prox_1();
+	status_prox_2 = get_prox_2();
+
 
 	// accendo i led in base ai sensori di prossimità
-	if(status_prox_1 == HIGH && last_prox_1 == HIGH){
-		digitalWrite(pin_led_1, LOW);
-	}else if(status_prox_1 == LOW && last_prox_1 == LOW){
+	if(status_prox_1){
 		digitalWrite(pin_led_1, HIGH);
-	}
-	last_prox_1 = status_prox_1;
-
-	if(status_prox_2 == HIGH && last_prox_2 == HIGH){
-		digitalWrite(pin_led_2, LOW);
-	}else if(status_prox_2 == LOW && last_prox_2 == LOW){
-		digitalWrite(pin_led_2, HIGH);
-	}
-	last_prox_2 = status_prox_2
-
-	if(status_prox_1 == HIGH || status_prox_2 == HIGH){
-		// se uno dei sensori non è attivo
-		// allora vado avanti a "registrare" il rumore di fondo:
-
-		// spengo il led centrale
-		digitalWrite(pin_led, LOW);
-		// prendo il rumore
-		noise = analogRead(pin_mic);
 	}else{
+		digitalWrite(pin_led_1, LOW);
+	}
 
+	if(status_prox_2){
+		digitalWrite(pin_led_2, HIGH);
+	}else{
+		digitalWrite(pin_led_2, LOW);
+	}
+
+	if(status_prox_1 && status_prox_2){
 		// altrimenti
 		// accendo il led in base a ciò che leggo dal microfono
 		int status_mic = analogRead(pin_mic);
@@ -121,6 +158,15 @@ void loop(){
 		// "rangifizzo" il valore del led per evitare il "flickeraggio".
 		// infine scrivo il valore ottenuto sul led
 		analogWrite(pin_led, rangify(status_led));
+
+	}else{
+		// se uno dei sensori non è attivo
+		// allora vado avanti a "registrare" il rumore di fondo:
+
+		// spengo il led centrale
+		digitalWrite(pin_led, LOW);
+		// prendo il rumore
+		noise = analogRead(pin_mic);
 	}
 
 	delay(LOOP_DELAY);
